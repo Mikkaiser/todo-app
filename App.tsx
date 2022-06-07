70a5fa899678c8d08b94ea2514721893d8d6de4f
Mikkaiser <mikael.simoes@al.senai.br>
2022-06-07 19:13:08 -0300

    add: LinearGradient

--- App.tsx
+++ App.tsx
@@ -1,12 +1,26 @@
 import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { LinearGradient } from 'expo-linear-gradient';
 import { StyleSheet, Text, View } from "react-native";
-import { Container } from "./mainStyle";
 import Welcome from "./src/screens/Welcome";
 
 export default function App() {
   return (
-    <Container>
-      <Welcome />
-    </Container>
+      <LinearGradient
+        // Button Linear Gradient
+        style={styles.container}
+        colors={['#002E8C', '#2D668F']}
+        start={{ x: 0, y: 0.2}}
+        end={{ x: 0, y: 2}}
+      >
+        <Welcome/>
+      </LinearGradient>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: '5%'
+  }
+})
